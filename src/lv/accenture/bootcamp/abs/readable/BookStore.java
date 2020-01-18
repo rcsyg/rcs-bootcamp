c1442e46385c83a95dbc83e284407daa75442f59
rcsyg <yura.grabovsky@gmx.com>
2020-01-18 11:25:07 +0200

    Bookstore adjusted

--- src/lv/accenture/bootcamp/abs/readable/BookStore.java
+++ src/lv/accenture/bootcamp/abs/readable/BookStore.java
@@ -2,13 +2,19 @@ package lv.accenture.bootcamp.abs.readable;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class BookStore {
 
     public static void main(String[] args) {
+
+        List<String> multipleAuthors = new ArrayList<>();
+        multipleAuthors.add("Oscar Wilde");
+        multipleAuthors.add("Alexander Pushkin");
+
         Readable journal = new Journal("Forbes", "Successful success...", "Forbes Media");
-        Readable fictionBook = new Fiction("The Picture of Dorian Gray", "A lot of text inside",
-                Collections.singletonList("Oscar Wilde"), "novel");
+        Readable fictionBook = new Fiction("The Picture of Dorian Gray",
+                "A lot of text inside", multipleAuthors, "novel");
         Readable nonFictionBook = new NonFiction("Thinking, Fast and Slow", "Subj.",
                 Collections.singletonList("Daniel Kahneman"), new ArrayList<>(), "GTD");
 
